b9e0b1b5e3c62757e3007ddbec7a2fd99ee9cc23
Philip Mottershead <philipmottershead@gmail.com>
2020-08-18 14:50:32 +0100

    Started from template code

---
+++ Android_App/app/src/main/java/uk/ac/aber/dcs/cascade/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package uk.ac.aber.dcs.cascade.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
